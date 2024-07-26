433699bd808472b2ec8dbfdf804698fe8415b18c
Default Box <134133080+dfltbox@users.noreply.github.com>
2024-07-26 20:51:33 +1000

    basic functionality

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/imroc/req/v3"
+)
+
+func main() {
+	const url = "http://localhost:11434/api/generate"
+	const model = "llama3.1"
+	const query = "names with 'jack'"
+	const prompt = "Generate passwords that contain" + query + ". Return 10 passwords in a valid array."
+
+	client := req.C()
+
+	type PromptT struct {
+		Model  string `json:"model"`
+		Prompt string `json:"prompt"`
+		Format string `json:"format"`
+		Stream bool   `json:"stream"`
+		System string `json:"system"`
+		Raw    bool   `json:"raw"`
+	}
+
+	Prompt := &PromptT{
+		Model:  model,
+		Prompt: prompt,
+		Stream: false,
+		System: "You are a password generating bot. Do not generate anything else. Make sure it is a valid array that can be used in a script",
+	}
+
+	resp, err := client.R().
+		SetBody(Prompt).Post(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+}
