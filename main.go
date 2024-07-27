24c03350f25a56bbcba4f10f3b8b82c32fbd9981
Default Box <134133080+dfltbox@users.noreply.github.com>
2024-07-27 23:44:45 +1000

    writing to output file now!

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/imroc/req/v3"
@@ -43,6 +44,18 @@ func clean(resp string) []string {
 	return result
 }
 
+func appendtoOutput(text string) {
+	outputFile := os.Args[1]
+	file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	if _, err := file.WriteString(text); err != nil {
+		log.Fatalf("failed to append text to file: %s", err)
+	}
+	defer file.Close()
+}
+
 func generate() {
 	client := req.C()
 
@@ -70,11 +83,16 @@ func generate() {
 	if err != nil {
 		log.Fatalf("Error parsing JSON: %v", err)
 	}
-
+	//for debug
+	response.Response = "['JackHammer88', 'JackSparrow123', 'JackDaniels77', 'JacksOrBetter11', 'JackOfAllTrades22', 'TheJackalope99', 'BlackJack777', 'CardSharkJack44', 'HighRollerJack55', 'LuckyJack999']"
 	words := clean(response.Response)
+	var appendedOutput string
 	for _, word := range words {
 		fmt.Println("Generated word", word)
+		appendedOutput = appendedOutput + word + "\n"
 	}
+	appendedOutput = strings.ReplaceAll(appendedOutput, "'", "")
+	appendtoOutput(appendedOutput)
 	fmt.Println(response.Response)
 }
 
